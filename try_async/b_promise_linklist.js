de3352304873dd06ccf270c340cb4d5f5488498c
jqxiong <jqxiong@trip.com>
2020-11-06 13:52:40 +0800

    BPromise.all

--- try_async/b_promise_linklist.js
+++ try_async/b_promise_linklist.js
@@ -74,6 +74,39 @@ class BPromise {
     catch(errCall) {
         this.promise.invoke('failCall', errCall);
     }
+    // 实现Promise.all, Promise.race
+    /**
+     * Promise.all, 将多个Promise包装成一个新的Promise
+     * 成功时，返回一个数组，数组分别是每一个promise的结果
+     * 失败时，返回最先reject的值
+     * Promise.all([pr1, pr2, pr3]).then().catch()
+     */
+    static all(promises) {
+        let rst = new Array(promises.length);
+        let hasErr = false;
+        let idx = 0;
+
+        return new BPromise((resolve, reject) => {
+            // all 里面的函数，不需要等待上一个结果的完成, 所以可以同时触发, 将不同的结果存入数组中
+            for (let i = 0; i < promises.length; i++) {
+                promises[i]
+                .then(data => {
+                    rst[i] = data;
+                    idx++;
+                    // 判断所有的promise都已经完成
+                    if (idx == rst.length) {
+                        resolve(rst);
+                    }
+                })
+                .catch(err => {
+                    if (!hasErr) {
+                        hasErr = true
+                        reject(err)
+                    }
+                })
+            }
+        })
+    }
 }
 
 // const bp = new BPromise((resolve, reject) => {
@@ -102,3 +135,34 @@ class BPromise {
 //         console.log('err')
 //         console.log(err)
 //     })
+
+const pr1 = new BPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({data: 'foo'})
+        // reject('error pr1')
+    }, 200)
+})
+
+const pr2 = new BPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve({ foo: 'bar' })
+    }, 500)
+})
+
+BPromise
+.all([
+    pr1,
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            // reject('error')
+            resolve('ok')
+        }, 300)
+    }),
+    pr2
+])
+.then(rst => {
+    console.log(rst)
+})
+.catch(err => {
+    console.log(err)
+})
