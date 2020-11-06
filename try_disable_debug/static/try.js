9d444e8ce667d309559302472bfb5198d7b0257b
jqxiong <jqxiong@trip.com>
2020-11-06 10:28:01 +0800

    fix debugger

--- try_disable_debug/static/try.js
+++ try_disable_debug/static/try.js
@@ -1,9 +1,13 @@
 // 静态资源js
 
-(function() {var a = new Date(); debugger; return new Date() - a > 100;}())
+(function debug() {
+    debugger
+    setTimeout(debug, 100)
+})()
 
-function aa() {
-    console.log(aa)
+function showList() {
+    document.write('可以使用无限循环的debugger来打断点, 阻止调试');
+    // 解决办法： 通过fiddler抓包替换文件, 移除debugger代码
 }
 
-aa();
+showList();
