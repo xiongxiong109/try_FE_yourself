35708f1c3000a9c99a6d0871713621be6e80b4ef
jqxiong <jqxiong@trip.com>
2020-11-09 16:43:30 +0800

    update template

--- try_node_server/src/template.js
+++ try_node_server/src/template.js
@@ -8,22 +8,51 @@
 const path = require('path')
 const fs = require('fs')
 
+// Simple JavaScript Templating
+// John Resig - http://ejohn.org/ - MIT Licensed
+/**
+ * 正常书写html
+ * <%  %> 嵌套js语句
+ * <%= %> 输出变量
+ */
+function tmpl(str, data){
+    // Figure out if we're getting a template, or if we need to
+    // load the template - and be sure to cache the result.
+    var fn = !/\W/.test(str) ?
+      cache[str] = cache[str] ||
+        tmpl(document.getElementById(str).innerHTML) :
+
+      // Generate a reusable function that will serve as a template
+      // generator (and which will be cached).
+      new Function("obj",
+        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+
+        // Introduce the data as local variables using with(){}
+        "with(obj){p.push('" +
+
+        // Convert the template into pure JavaScript
+        str
+          .replace(/[\r\t\n]/g, " ")
+          .split("<%").join("\t")
+          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+          .replace(/\t=(.*?)%>/g, "',$1,'")
+          .split("\t").join("');")
+          .split("%>").join("p.push('")
+          .split("\r").join("\\'")
+      + "');}return p.join('');");
+
+    // Provide some basic currying to the user
+    return data ? fn( data ) : fn;
+  }
+
 function render(temStr, dataInfo) {
+    let str = temStr
     try {
-        let str = temStr.replace(/\n/g, '\\');
-        str = '\'' + str + '\''
-        // str = str.replace(/\{\{\s?each\s(\w+)\sin\s(\w+)\s?\}\}/g, '\' + for (let item = 0; item < dataInfo.$2.length; item++) { + \'');
-        // str = str.replace(/(\{\{\s?\/each\s?\}\})/g, '\' + } + \'')
-        // 替换{{}}
-        str = str.replace(/\{\{(\w+)\}\}/g, '\' + (dataInfo.$1 || "") + \'');
-        console.log(str)
-        // 用eval函数对替换后对字符串执行js运算
-        const rst = eval(str)
-        return rst;
+        str = tmpl(temStr, dataInfo);
     } catch (e) {
         console.log(e)
     }
-    return temStr
+    return str
 }
 
 // view中间件
